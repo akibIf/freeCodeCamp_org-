5ed0b753b347f1909292a55432b5663b995b3b99
Akib Iftakher <akibiftakher@gmail.com>
2023-12-06 01:07:44 +0600

    Add two project

---
+++ Go_Programming_Tutorial_3_Beginner_Projects/go-file-encryption/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	// "github.com/akhilsharma90/file-encrypt/filecrypt"
+	"golang.org/x/term"
+
+	// "github.com/akhilsharma90/file-encryption/filecrypt"
+	// "github.com/akhilsharma90/file-encrypt/filecrypt"
+	// "github.com/akhilsharma90/file-encrypt/filecrypt"
+	// "\filecrypt"
+	"go-file-encryption/filecrypt"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printHelp()
+		os.Exit(0)
+	}
+	function := os.Args[1]
+
+	switch function {
+	case "help":
+		printHelp()
+
+	case "encrypt":
+		encryptHandle()
+
+	case "decrypt":
+		decryptHandle()
+
+	default:
+		fmt.Println("Run encrypt to encrypt a file, and decrypt to decrypt a file")
+		os.Exit(1)
+
+	}
+}
+func printHelp() {
+	fmt.Println("CryptoGo")
+	fmt.Println("Simple file encrypter for your day-to-day needs.")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\tCryptoGo encrypt /path/to/your/file")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("")
+	fmt.Println("\t encrypt\tEncrypts a file given a password")
+	fmt.Println("\t decrypt\tTries to decrypt a file using a password")
+	fmt.Println("\t help\t\tDisplays help text")
+	fmt.Println("")
+
+}
+func encryptHandle() {
+
+	if len(os.Args) < 3 {
+		println("missing the path to the file. For more infor, run go run. help")
+		os.Exit(0)
+	}
+
+	file := os.Args[2]
+	if !validateFile(file) {
+		panic("file not found")
+	}
+
+	password := getPassword()
+	fmt.Println("\nEncrypting...")
+	filecrypt.Encrypt(file, password)
+	fmt.Println("\nfile successfully protected")
+}
+
+func decryptHandle() {
+	if len(os.Args) < 3 {
+		println("missing the path to the file. For more infor, run go run. help")
+		os.Exit(0)
+	}
+	file := os.Args[2]
+	if !validateFile(file) {
+		panic("file not found")
+	}
+
+	fmt.Print("Enter password:")
+	// password :=
+	password, _ := term.ReadPassword(0)
+	fmt.Println("\nDecrypting...")
+	filecrypt.Decrypt(file, password)
+	fmt.Println("\nfile successfully decrypted")
+}
+
+func getPassword() []byte {
+	fmt.Println("Enter password")
+	password, _ := term.ReadPassword(0)
+	fmt.Println("\nConfrim password: ")
+	password2, _ := term.ReadPassword(0)
+
+	if !validatePassword(password, password2) {
+		fmt.Println("\nPasswords do not match. Please try again\n ")
+	}
+	return password
+}
+
+func validatePassword(password1 []byte, password2 []byte) bool {
+	if !bytes.Equal(password1, password2) {
+		return false
+	}
+
+	return true
+
+}
+
+func validateFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
